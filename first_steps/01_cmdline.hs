96a3cba43cfc08065770f60aeb2874f3395a3f5b
abiduzz420 <abiduzair420@gmail.com>
2019-03-10 16:53:26 +0530

    add two nums from cmd

--- first_steps/01_cmdline.hs
+++ first_steps/01_cmdline.hs
@@ -4,4 +4,4 @@ import System.Environment
 main :: IO()
 main = do
     args <- getArgs
-    putStrLn ("Hello! " ++ args !! 0 ++ " " ++ args !! 1)
+    putStrLn ("The sum is " ++ show (read (args !! 0) + read (args !! 1)))
