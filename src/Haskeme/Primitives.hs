971098984f82df83be53acdb75279a7ea9a6dada
abiduzz420 <abiduzair420@gmail.com>
2019-05-26 10:40:16 +0530

    rearranging code

--- src/Haskeme/Primitives.hs
+++ src/Haskeme/Primitives.hs
@@ -101,6 +101,42 @@ unpackNum (String n) =
 unpackNum (List [n]) = unpackNum n
 unpackNum notNum     = throwError $ TypeMismatch "number" notNum
 
+data Unpacker = forall a. Eq a => AnyUnpacker (LispVal -> ThrowsError a)
+
+unpackEquals :: LispVal -> LispVal -> Unpacker -> ThrowsError Bool
+unpackEquals arg1 arg2 (AnyUnpacker unpacker) =
+  do
+      unpackerArg1 <- unpacker arg1
+      unpackerArg2 <- unpacker arg2
+      return $ unpackerArg1 == unpackerArg2
+    `catchError` const (return False)
+
+equal :: [LispVal] -> ThrowsError LispVal
+equal [arg1, arg2] = do
+  primitiveEquals <- liftM or $ mapM
+    (unpackEquals arg1 arg2)
+    [AnyUnpacker unpackNum, AnyUnpacker unpackStr, AnyUnpacker unpackBool]
+  eqvEquals <- eqv [arg1, arg2]
+  return $ Bool $ primitiveEquals || let (Bool x) = eqvEquals in x
+equal badArgList = throwError $ NumArgs 2 badArgList
+
+eqv :: [LispVal] -> ThrowsError LispVal
+eqv [Bool   arg1, Bool arg2  ] = return $ Bool $ arg1 == arg2
+eqv [Number arg1, Number arg2] = return $ Bool $ arg1 == arg2
+eqv [String arg1, String arg2] = return $ Bool $ arg1 == arg2
+eqv [Atom   arg1, Atom arg2  ] = return $ Bool $ arg1 == arg2
+eqv [DottedList xs x, DottedList ys y] =
+  eqv [List (xs ++ [x]), List (ys ++ [y])]
+eqv [List arg1, List arg2] = return $ Bool $ length arg1 == length arg2 && all
+  eqvPair
+  (zip arg1 arg2)
+ where
+  eqvPair (x, y) = case eqv [x, y] of
+    Left  err        -> False
+    Right (Bool val) -> val
+eqv [_, _]     = return $ Bool False
+eqv badArgList = throwError $ NumArgs 2 badArgList
+
 car :: [LispVal] -> ThrowsError LispVal
 car [List (x : xs)        ] = return x
 car [DottedList (x : xs) _] = return x
@@ -120,42 +156,6 @@ cons [x , List xs        ] = return $ List $ x : xs
 cons [x , DottedList xs y] = return $ DottedList (x : xs) y
 cons [x1, x2             ] = return $ DottedList [x1] x2
 cons badArgList            = throwError $ NumArgs 2 badArgList
-
-eqv :: [LispVal] -> ThrowsError LispVal
-eqv [Bool   arg1, Bool arg2  ] = return $ Bool $ arg1 == arg2
-eqv [Number arg1, Number arg2] = return $ Bool $ arg1 == arg2
-eqv [String arg1, String arg2] = return $ Bool $ arg1 == arg2
-eqv [Atom   arg1, Atom arg2  ] = return $ Bool $ arg1 == arg2
-eqv [DottedList xs x, DottedList ys y] =
-  eqv [List (xs ++ [x]), List (ys ++ [y])]
-eqv [List arg1, List arg2] = return $ Bool $ length arg1 == length arg2 && all
-  eqvPair
-  (zip arg1 arg2)
- where
-  eqvPair (x, y) = case eqv [x, y] of
-    Left  err        -> False
-    Right (Bool val) -> val
-eqv [_, _]     = return $ Bool False
-eqv badArgList = throwError $ NumArgs 2 badArgList
-
-data Unpacker = forall a. Eq a => AnyUnpacker (LispVal -> ThrowsError a)
-
-unpackEquals :: LispVal -> LispVal -> Unpacker -> ThrowsError Bool
-unpackEquals arg1 arg2 (AnyUnpacker unpacker) =
-  do
-      unpackerArg1 <- unpacker arg1
-      unpackerArg2 <- unpacker arg2
-      return $ unpackerArg1 == unpackerArg2
-    `catchError` const (return False)
-
-equal :: [LispVal] -> ThrowsError LispVal
-equal [arg1, arg2] = do
-  primitiveEquals <- liftM or $ mapM
-    (unpackEquals arg1 arg2)
-    [AnyUnpacker unpackNum, AnyUnpacker unpackStr, AnyUnpacker unpackBool]
-  eqvEquals <- eqv [arg1, arg2]
-  return $ Bool $ primitiveEquals || let (Bool x) = eqvEquals in x
-equal badArgList = throwError $ NumArgs 2 badArgList
 
 -- io primitives reacting to outside world
 
